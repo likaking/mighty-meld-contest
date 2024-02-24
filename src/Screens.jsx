d0f475c1e49ebcba0f8e8100acd8a83163122a14
rich Nigga <kunaplanet@gmail.com>
2024-02-24 23:53:25 +0100

    Mighty Meld

--- src/Screens.jsx
+++ src/Screens.jsx
@@ -117,7 +117,7 @@ export function PlayScreen({ end }) {
         confetti({
           ticks: 100,
         });
-		setTimeout(()=>{matched.play()},200)
+		setTimeout(()=>{matched.play()},220)
         newState = "matched";
       }
 
